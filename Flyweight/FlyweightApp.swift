e8e11b2115bcb34fe6cf3d32d8c92777595abdeb
John Clayton <john@fivesquare.net>
2023-03-31 14:29:44 -0400

    Initial

---
+++ Flyweight/FlyweightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyweightApp.swift
+//  Flyweight
+//
+//  Created by John Clayton on 2023/3/31.
+//
+
+import SwiftUI
+
+@main
+struct FlyweightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
