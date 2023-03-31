e9a8af007b3649109f45ed6f47cece9554c8a69e
John Clayton <john@fivesquare.net>
2023-03-31 14:45:18 -0400

    Print env

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  Flyweight
+#
+#  Created by John Clayton on 2023/3/31.
+#  
+
+env
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Flyweight
+#
+#  Created by John Clayton on 2023/3/31.
+#  
+
+env
